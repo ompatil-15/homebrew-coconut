8416cc6fa493e23ed483d23ffbe3584ac9cbba4d
Om Patil <patilom001@gmail.com>
2025-11-12 23:46:57 +0530

    feat: add coconut formula

---
+++ Formula/coconut.rb
@@ -0,0 +1,17 @@
+class Coconut < Formula
+  desc "Secure, local-first CLI password manager with Zero Knowledge Architecture"
+  homepage "https://github.com/ompatil-15/coconut"
+  url "https://github.com/ompatil-15/coconut/archive/v1.0.0.tar.gz"
+  sha256 "f52445f8c71b59b9d27752a520df49436f2a57c1a3048c2ee0ed25059597dad4"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "."
+  end
+
+  test do
+    system "#{bin}/coconut", "--version"
+  end
+end
