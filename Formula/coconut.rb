b6f6bb4198e81a3242ddfffc9968e70afcf2936f
Om Patil <patilom001@gmail.com>
2025-11-13 00:24:20 +0530

    chore: update sha256 hash

--- Formula/coconut.rb
+++ Formula/coconut.rb
@@ -2,7 +2,7 @@ class CoconutPm < Formula
   desc "Secure, local-first CLI password manager with Zero Knowledge Architecture"
   homepage "https://github.com/ompatil-15/coconut"
   url "https://github.com/ompatil-15/coconut/archive/v1.0.0.tar.gz"
-  sha256 "440d5b84ade063612157aef92548cd00cac18e704310229e7f14da3e15ce76e4"
+  sha256 "6b96f4c38f3f18db467ba1a28a831d22cbac2f2160c0014f6726c6c7ffce8ca6`"
   license "Apache-2.0"
 
   depends_on "go" => :build
